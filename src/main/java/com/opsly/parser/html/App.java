8d3d5ea976b320b90b8f3178e511ed9019becb3f
aymens <sayhi.aymen@gmail.com>
2021-01-22 21:21:00 +0100

    parser and code to run the parsing tree gui

---
+++ src/main/java/com/opsly/parser/html/App.java
@@ -0,0 +1,60 @@
+package com.opsly.parser.html;
+
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.antlr.v4.gui.Trees;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+
+public class App {
+
+	public static void main(String[] args) throws Exception {
+
+		String fileURL = getURL(args);
+		
+		System.out.println("Parse tree of " + fileURL);
+		
+		CharStream charStream = CharStreams.fromStream(new URL(fileURL).openStream(),
+				Charset.defaultCharset());
+
+		// Create an Lexer that receives the char stream
+		HTMLLexer lexer = new HTMLLexer(charStream);
+
+		// Create a token stream from the lexer
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+		tokens.fill();
+
+		// Create a parser that receives the token stream
+		HTMLParser parser = new HTMLParser(tokens);
+
+		parser.setBuildParseTree(true);
+
+		parser.setTokenStream(tokens);
+
+		ParserRuleContext tree = parser.htmlDocument();
+
+		Trees.inspect(tree, parser);
+	}
+
+	private static String getURL(String[] args) {
+		String url = null;
+		if(args.length !=1) {
+			System.out.println("Wrong arguments number!\n "//
+					+ "This program expects one single string argument reprenting a url of a html page.\n"//
+					+ "Examples:\n"
+					+ "(html document on the internet)"
+					+ "java -jar path_to_jar/html-parser-jar-with-dependencies.jar \"https://www.wikipedia.org/\""
+					+ "(local html file with relative path)"
+					+ "java -jar path_to_jar/html-parser-jar-with-dependencies.jar \"file:./pom.xml\"");
+			System.exit(1);
+		} else {
+			url = args[0];
+		}
+		return url;
+	}
+
+}
